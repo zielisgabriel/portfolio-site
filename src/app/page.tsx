b6d569728f1ef7b24012b8e7b6acc952b978bb24
zielisgabriel <josegabrielas0586@gmail.com>
2025-04-02 09:09:05 -0300

    feat: Home page presentation prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,57 @@
-import Image from "next/image";
+import { DM_Serif_Display, M_PLUS_1_Code } from "next/font/google";
+import { ParticlesBackground } from "@/components/ParticlesBackground";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+
+const dmSerifDisplay = DM_Serif_Display({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-dm-serif-display-serif",
+})
+
+const mPlus1Code = M_PLUS_1_Code({
+  subsets: ["latin"],
+  variable: "--font-m-plus-1-code-sans-serif",
+})
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main>
+      <section className="h-screen w-full">
+        <ParticlesBackground />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="grid grid-cols-2 items-center justify-center h-full max-w-7xl m-auto">
+          <div>
+            <h1 className={`${dmSerifDisplay.className} text-9xl flex flex-col`}>
+              José
+              <span className="flex items-center gap-2 after:w-0.5 after:h-24 after:bg-white after:block after:animate-vertical-bar-to-write">
+                Gabriel
+              </span>
+            </h1>
+
+            <h2 className={`${mPlus1Code.className} text-xl opacity-80`}>
+              {"<div>"}<br />
+              &ensp; {"<p>"}<br />
+              &ensp; &ensp; Meu portifólio {"<span>profissional!</span>"}<br />
+              &ensp; {"</p>"}<br />
+              {"</div>"}<br />
+            </h2>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="absolute flex flex-col items-center bottom-4 left-[50%] translate-[-50%] animate-item-float opacity-70">
+          <span>Swipe down</span>
+          <ChevronDownIcon width={32} height={32} />
+        </div>
+      </section>
+
+      <section className="bg-secondary py-8">
+        <div className="w-7xl m-auto">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dignissimos atque, obcaecati debitis adipisci tenetur eos reprehenderit earum molestiae, dolores neque temporibus. Dolores temporibus veritatis commodi numquam laudantium. Illum, ex.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dignissimos atque, obcaecati debitis adipisci tenetur eos reprehenderit earum molestiae, dolores neque temporibus. Dolores temporibus veritatis commodi numquam laudantium. Illum, ex.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dignissimos atque, obcaecati debitis adipisci tenetur eos reprehenderit earum molestiae, dolores neque temporibus. Dolores temporibus veritatis commodi numquam laudantium. Illum, ex.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dignissimos atque, obcaecati debitis adipisci tenetur eos reprehenderit earum molestiae, dolores neque temporibus. Dolores temporibus veritatis commodi numquam laudantium. Illum, ex.</p>
+        </div>
+      </section>
+    </main>
   );
 }
