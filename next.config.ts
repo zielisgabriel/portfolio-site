d7815b9889e96ca4169e2ff48b61bd55c2ad04bf
zielisgabriel <josegabrielas0586@gmail.com>
2025-04-03 13:07:21 -0300

    raw: Remote patterns config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
