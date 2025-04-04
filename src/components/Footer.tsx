81ad23334e29e659c9bac13210e37c7d3f446861
zielisgabriel <josegabrielas0586@gmail.com>
2025-04-04 16:18:58 -0300

    style: Change text weight to extra-light

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <footer className="flex justify-center h-8 bg-secondary">
+        <footer className="flex justify-center h-8 bg-secondary font-extralight text-sm">
             <p>Footer example</p>
         </footer>
     );
