bcb637d9d7038cfff666a64dd73ef6eed61bff67
zielisgabriel <josegabrielas0586@gmail.com>
2025-04-27 18:55:51 -0300

    feat: Added new Languages and Frameworks experience

--- assets/js/addCardProjects.js
+++ assets/js/addCardProjects.js
@@ -2,6 +2,7 @@ const technologiesImg = {
     html: "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white",
     css: "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white",
     js: "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black",
+    nextjs: "https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white",
     ts: "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
     node: "https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=node.js&logoColor=white",
     react: "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
