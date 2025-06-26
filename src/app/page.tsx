ef65abe8bd84768a201f6b57b8a45224c62458ae
zielisgabriel <josegabrielas0586@email.com>
2025-06-26 12:38:41 -0300

    Change all home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,79 @@
-import { DM_Serif_Display, M_PLUS_1_Code } from "next/font/google";
-import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { Cinzel } from "next/font/google";
 import { Metadata } from "next";
+import { ParticlesBackground } from "@/components/ParticlesBackground";
+import Image from "next/image";
 
-const dmSerifDisplay = DM_Serif_Display({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-dm-serif-display-serif",
-})
+import Astronaut from "../../public/astronaut.png"
+import { GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
-const mPlus1Code = M_PLUS_1_Code({
+const cinzel = Cinzel({
+  weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
-  variable: "--font-m-plus-1-code-sans-serif",
 })
 
 export const metadata: Metadata = {
-  title: "José Gabriel | Portfólio",
+  title: "Zielis Gabriel | Portfólio",
   description: "Meu portfólio de Desenvolvedor Web profissional."
 }
 
 export default function Home() {
   return (
     <main>
+      <ParticlesBackground />
 
+      <section className="overflow-hidden" id="home">
+        <div className="flex flex-col items-center justify-center min-h-screen max-w-7xl mx-auto">
+          <div className="relative text-center">
+            <Image
+              src={Astronaut}
+              alt="Astronaut"
+              className="absolute w-xl animate-item-float -right-100 -bottom-150"
+            />
+            
+            <h1 className={`${cinzel.className} text-7xl`}>
+              Zielis Gabriel
+            </h1>
+
+            <h2 className={`${cinzel.className} text-2xl`}>
+              Full-stack developer
+            </h2>
+          </div>
+
+          <ul className="flex gap-4 mt-2">
+            <li>
+              <Link
+                href="https://github.com/zielisgabriel"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer hover:opacity-80"
+              >
+                <GitHubLogoIcon className="w-8 h-8" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/josgabrielalmeida/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer hover:opacity-80"
+              >
+                <LinkedInLogoIcon className="w-8 h-8" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.instagram.com/zielis085/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer hover:opacity-80"
+              >
+                <InstagramLogoIcon className="w-8 h-8" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
