eeffc74a3d09477c594255eeb5b2c30dc035598d
José Gabriel <josegabrielas0586@gmail.com>
2025-06-28 02:52:20 -0300

    Delete assets directory

--- assets/js/btnMobile.js
+++
@@ -1,5 +0,0 @@
-const btnMobile = document.getElementById('btnMobile')
-const navMobile = document.querySelector('.nav-mobile')
-btnMobile.addEventListener('click', () => {
-    navMobile.classList.toggle('show')
-})
--- assets/js/scrollHeader.js
+++
@@ -1,7 +0,0 @@
-const header = document.getElementById('header')
-window.addEventListener('DOMContentLoaded', () => {
-    header.classList.toggle('scrolling', window.scrollY > 0)
-})
-window.addEventListener('scroll', () => {
-    header.classList.toggle('scrolling', window.scrollY > 100)
-})
