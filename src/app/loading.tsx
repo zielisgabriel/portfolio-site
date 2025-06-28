e2245ac70f5448aa5cbd4254eb87960aeb9f73e9
zielisgabriel <josegabrielas0586@email.com>
2025-06-28 02:16:25 -0300

    feat: Loading screen added

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { DotsLoader } from "@/components/DotsLoader";
+
+export default function Loading() {
+    return (
+        <div className="absolute left-0 top-0 z-100 flex flex-col justify-center items-center w-full h-screen bg-background">
+            <DotsLoader />
+        </div>
+    );
+}
