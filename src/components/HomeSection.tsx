69c4506e356cb1e029eb98a1e7fc8ca7b70f8000
zielisgabriel <josegabrielas0586@email.com>
2025-07-03 12:12:29 -0300

    feat: View Curriculum button added

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -13,56 +13,70 @@ const cinzel = Cinzel({
 export function HomeSection() {
     return (
         <section id="home" className="md:overflow-visible overflow-visible animate-fade-in">
-            <div className="flex flex-col items-center justify-center min-h-screen max-w-7xl mx-auto">
-            <div className="relative text-center">
-                <Image
-                    src={Astronaut1}
-                    alt="Astronaut"
-                    className="absolute md:-bottom-135 md:-right-100 animate-item-float sm:-bottom-130 -bottom-105 -right-2 z-50"
-                    priority
-                />
-                
-                <h1 className={`${cinzel.className} md:text-7xl sm:text-5xl text-4xl`}>
-                Zielis Gabriel
-                </h1>
+            <div className="flex flex-col items-center justify-center h-screen max-h-screen min-h-[700px] max-w-7xl mx-auto">
+                <div className="relative text-center">
+                    <Image
+                        src={Astronaut1}
+                        alt="Astronaut"
+                        className="absolute md:-bottom-135 md:-right-100 animate-item-float sm:-bottom-145 -bottom-120 -right-2 z-50"
+                        priority
+                    />
+                    
+                    <h1 className={`${cinzel.className} md:text-7xl sm:text-5xl text-4xl`}>
+                    Zielis Gabriel
+                    </h1>
 
-                <h2 className={`${cinzel.className} md:text-2xl text-xl`}>
-                Full-stack developer
-                </h2>
-            </div>
+                    <h2 className={`${cinzel.className} md:text-2xl text-xl`}>
+                    Full-stack developer
+                    </h2>
+                </div>
 
-            <ul className="flex gap-4 mt-2 md:mb-0 mb-60">
-                <li>
+                <ul className="flex gap-4 mt-2">
+                    <li>
+                        <Link
+                            href="https://github.com/zielisgabriel"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="cursor-pointer hover:opacity-80"
+                        >
+                            <GitHubLogoIcon className="md:w-8 md:h-8 w-6 h-6" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="https://www.linkedin.com/in/josgabrielalmeida/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="cursor-pointer hover:opacity-80"
+                        >
+                            <LinkedInLogoIcon className="md:w-8 md:h-8 w-6 h-6" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="https://www.instagram.com/zielis085/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="cursor-pointer hover:opacity-80"
+                        >
+                            <InstagramLogoIcon className="md:w-8 md:h-8 w-6 h-6" />
+                        </Link>
+                    </li>
+                </ul>
+
                 <Link
-                    href="https://github.com/zielisgabriel"
+                    href="https://docs.google.com/document/d/1yMuvbtr0Nx3zqZJLSM_-SZwGLFiD2XCai85EcAqrv9M/edit?usp=sharing"
+                    title="Curriculum"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="cursor-pointer hover:opacity-80"
+                    className="mt-4 md:mb-0 mb-60"
                 >
-                    <GitHubLogoIcon className="md:w-8 md:h-8 w-6 h-6" />
+                    <button className="bg-foreground text-background py-2 px-4 sm:text-xl text-md font-bold rounded-full cursor-pointer hover:bg-foreground/10 hover:text-foreground">
+                        <p>
+                            View Curriculum
+                        </p>
+                    </button>
                 </Link>
-                </li>
-                <li>
-                <Link
-                    href="https://www.linkedin.com/in/josgabrielalmeida/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="cursor-pointer hover:opacity-80"
-                >
-                    <LinkedInLogoIcon className="md:w-8 md:h-8 w-6 h-6" />
-                </Link>
-                </li>
-                <li>
-                <Link
-                    href="https://www.instagram.com/zielis085/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="cursor-pointer hover:opacity-80"
-                >
-                    <InstagramLogoIcon className="md:w-8 md:h-8 w-6 h-6" />
-                </Link>
-                </li>
-            </ul>
             </div>
         </section>
     );
