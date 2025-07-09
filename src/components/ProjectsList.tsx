408982e983f0538b74a8573538dfba0e81581934
zielisgabriel <josegabrielas0586@email.com>
2025-07-09 16:53:14 -0300

    fix: remove scroll bar

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -93,7 +93,7 @@ export function ProjectsList() {
                             <Dialog.Overlay className="fixed z-10 inset-0 bg-[rgba(0,0,0,0.5)] backdrop-blur-[4px]" />
 
                             <Dialog.Portal>
-                                <Dialog.Content className="fixed z-50 flex flex-col gap-4 bg-primary left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded p-2 pt-14 focus:outline-none sm:w-md w-[96%] max-h-[80vh] overflow-y-scroll border border-border-color">
+                                <Dialog.Content className="fixed z-50 flex flex-col gap-4 bg-primary left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded p-2 pt-14 focus:outline-none sm:w-md w-[96%] max-h-[80vh] overflow-y-scroll border border-border-color [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:bg-foreground [&::-webkit-scrollbar-thumb]:rounded [&::-webkit-scrollbar-track]:bg-background/0">
                                     <div className="flex justify-center items-center object-cover">
                                         {
                                             (project.imageUrl !== null) && (
