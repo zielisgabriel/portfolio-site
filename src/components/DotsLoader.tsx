224bf0f50e884a1c4d03c6689e77800f8feba1b3
zielisgabriel <josegabrielas0586@email.com>
2025-12-28 19:50:26 -0300

    refactor: remove unused components and types for a cleaner codebase

--- src/components/DotsLoader.tsx
+++
@@ -1,9 +0,0 @@
-export function DotsLoader() {
-    return (
-        <div  className="flex items-center mt-12 gap-1.5">
-            <div className="w-1.5 h-1.5 bg-white rounded-full animate-dot-float-loader"></div>
-            <div className="w-1.5 h-1.5 bg-white rounded-full animate-dot-float-loader delay-50"></div>
-            <div className="w-1.5 h-1.5 bg-white rounded-full animate-dot-float-loader delay-100"></div>
-        </div>
-    );
-}
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-    return (
-        <footer className="bg-background py-2">
-            <p className="opacity-60 text-sm text-center">
-                &copy; 2025 Zielis. All rights reserved
-            </p>
-        </footer>
-    );
-}
