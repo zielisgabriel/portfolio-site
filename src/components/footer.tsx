c454d87828af89880938f4b857d323706b3af302
zielisgabriel <josegabrielas0586@email.com>
2025-12-28 19:51:58 -0300

    feat: add Footer component for site footer with copyright information

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <footer className="bg-background py-2">
+            <p className="opacity-60 text-sm text-center">
+                &copy; 2025 Zielis. All rights reserved
+            </p>
+        </footer>
+    );
+}
