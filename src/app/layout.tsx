4c99dfb731977346278f50a4e165cd8a8c7ba94b
zielisgabriel <josegabrielas0586@email.com>
2026-01-03 20:18:52 -0300

    fix: update HTML language attribute to English and clean up layout formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { ProgressBarScroll } from "@/components/progress-bar-scroll";
-import { ReactLenis } from "lenis/react"
+import { ReactLenis } from "lenis/react";
 import { CustomCursor } from "@/components/custom-cursor";
 
 export default function RootLayout({
@@ -11,32 +11,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Libertinus+Math&display=swap" rel="stylesheet" />
         <link href="/logo-zls.png" rel="icon" />
       </head>
 
-      <body className={`antialiased dark`}>
-          <Header />
-          
-          <ProgressBarScroll />
-          
-          <CustomCursor />
-
-          <ReactLenis
-            root={true}
-            options={{
-              duration: 2,
-              wheelMultiplier: 3
-            }}
-          />
-
-          {children}
-
-          <Footer />
+      <body className="antialiased dark">
+        <Header />
+        <ProgressBarScroll />
+        <CustomCursor />
+        <ReactLenis
+          root={true}
+          options={{
+            duration: 2,
+            wheelMultiplier: 3
+          }}
+        />
+        {children}
+        <Footer />
       </body>
     </html>
   );
