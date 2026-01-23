419e932257060ad449edc7eccfc48551fbe6cda5
zielisgabriel <josegabrielas0586@email.com>
2026-01-22 21:29:43 -0300

    feat: implement RootLayout component with locale support and dynamic rendering

---
+++ src/app/[lang]/layout.tsx
@@ -0,0 +1,49 @@
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+import { ProgressBarScroll } from "@/components/progress-bar-scroll";
+import { ReactLenis } from "lenis/react";
+import { CustomCursor } from "@/components/custom-cursor";
+import "@/css/globals.css";
+import { getDictionary, hasLocale } from "./dictionaries";
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  return [{ lang: 'pt' }, { lang: 'en' }];
+}
+
+export default async function RootLayout({
+  children,
+  params
+}: Readonly<{
+  children: React.ReactNode,
+  params: Promise<{lang: string}>
+}>) {
+  const {lang} = await params;
+  if (!hasLocale(lang)) notFound();
+  const dict = await getDictionary(lang);
+
+  return (
+    <html lang={lang}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="/logo-zls.png" rel="icon" />
+      </head>
+
+      <body className="antialiased dark">
+        <Header dict={dict} />
+        <ProgressBarScroll />
+        <CustomCursor />
+        <ReactLenis
+          root={true}
+          options={{
+            duration: 2,
+            wheelMultiplier: 3
+          }}
+        />
+        {children}
+        <Footer dict={dict} />
+      </body>
+    </html>
+  );
+}
