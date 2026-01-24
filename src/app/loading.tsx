fbd76b5c1fec7c6100e315a87e8f7c34ebe06eea
zielisgabriel <josegabrielas0586@email.com>
2026-01-24 03:31:56 -0300

    feat: remove loading component as it is no longer needed

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { DotsLoader } from "@/components/dots-loader";
-
-export default function Loading() {
-    return (
-        <div className="absolute left-0 top-0 z-100 flex flex-col justify-center items-center w-full h-screen bg-background">
-            <DotsLoader />
-        </div>
-    );
-}
