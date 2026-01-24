c890ec7f57aad1a4389e5a805b92a940785b244c
zielisgabriel <josegabrielas0586@email.com>
2026-01-24 13:32:49 -0300

    feat: remove Project interface definition from project.d.ts

--- src/@types/project.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Project {
-    id: number;
-    title: string;
-    description: string;
-    imageUrl: string | null;
-    websiteUrl?: string;
-    repositories?: { type: string; url: string }[] | null;
-    technologies: string[];
-}
