cf8ef13b5605210f2cfdb565c3be713b4f7f5773
zielisgabriel <josegabrielas0586@gmail.com>
2026-04-25 06:31:28 -0300

    feat: enable React Compiler

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   cacheComponents: true,
+  reactCompiler: true,
   images: {
     remotePatterns: [
       {
