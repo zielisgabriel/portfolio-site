036cb0ca0c8f475061ee7c62584854543d85e34a
zielisgabriel <josegabrielas0586@gmail.com>
2026-04-25 06:32:44 -0300

    refactor: change load icon

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,10 +1,10 @@
-import { LoaderIcon } from "lucide-react"
+import { Loader2Icon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
 function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
   return (
-    <LoaderIcon
+    <Loader2Icon
       role="status"
       aria-label="Loading"
       className={cn("size-4 animate-spin", className)}
